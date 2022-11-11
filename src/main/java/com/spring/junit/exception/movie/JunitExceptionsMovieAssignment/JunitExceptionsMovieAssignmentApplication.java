de4b26d62635317dbeb07a8c57125c739fec72a1
prathamkush <Pratham.Kushwah@blueyonder.com>
2022-11-11 09:21:39 +0530

    init project

---
+++ src/main/java/com/spring/junit/exception/movie/JunitExceptionsMovieAssignment/JunitExceptionsMovieAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.spring.junit.exception.movie.JunitExceptionsMovieAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitExceptionsMovieAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitExceptionsMovieAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/junit/exception/movie/JunitExceptionsMovieAssignment/JunitExceptionsMovieAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.junit.exception.movie.JunitExceptionsMovieAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitExceptionsMovieAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
